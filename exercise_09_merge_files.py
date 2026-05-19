c0a74f1e3dd6157c32083c31716325117e38eedd
santiagoyamamoto <santiagoyamamoto08@gmail.com>
2026-05-19 16:23:16 -0300

    Update exercise_09_merge_files.py

--- exercise_09_merge_files.py
+++ exercise_09_merge_files.py
@@ -32,4 +32,12 @@ def merge_files(file1, file2, output):
         # hola
         # mundo
     """
-    pass  # Reemplazar con tu implementación
+
+    with open (file1, "r") as archivo:
+        contenido = archivo.read()
+
+    with open (file2, "r") as archivo2:
+        contenido2 = archivo2.read()
+
+    with open (output, "w") as final:
+        final.write(contenido + contenido2)
